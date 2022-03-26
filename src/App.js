91c15f2d534292859f036f8e75a7a5cd4e540224
Paxian-Pi <paxian.pi@gmail.com>
2022-03-26 18:32:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+import './App.css';
+import MovieCard from "./MovieCard";
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=a9ab7f9d';
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getPrviousSearchedMovie = localStorage.getItem('searchTerm');
+
+  useEffect(() => {
+    // Load last searched movie
+    searchMovies(getPrviousSearchedMovie)
+  }, [getPrviousSearchedMovie]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+
+    const data = await response.json();
+
+    console.log(data.Search);
+    setMovies(data.Search);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='Search'
+          onClick={() => {
+            searchMovies(searchTerm)
+            localStorage.setItem('searchTerm', searchTerm)
+          }}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {
+                movies.map((movie) => {
+                  return <MovieCard key={movie.imdbID} mov={movie} />
+                })
+              }
+            </div>
+          )
+          : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>
   );
 }
